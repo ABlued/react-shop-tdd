57c6adcc712763b9d63a6130f6f3f30221389ff2
ABlued <53801395+ABlued@users.noreply.github.com>
2021-12-12 21:17:21 +0900

    Products, Type 컴포넌트 테스트 및 구현

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Type from './pages/OrderPage/Type';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Type />
   </React.StrictMode>,
   document.getElementById('root')
 );
