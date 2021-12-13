28e2280f9ceafb87c4f4eb5878ac4aa7136c665c
ABlued <53801395+ABlued@users.noreply.github.com>
2021-12-14 00:02:25 +0900

    usecontext로 컴포넌트간의 데이터 연결하기

--- src/pages/OrderPage/Type.js
+++ src/pages/OrderPage/Type.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import Products from './Products';
 import ErrorBanner from '../../components/ErrorBanner';
 import Options from './Options';
+import { OrderContext } from '../../contexts/OrderContext';
 
 function Type({ orderType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDatas, updateItemCount] = useContext(OrderContext);
   useEffect(() => {
     loadItems(orderType);
   }, [orderType]);
@@ -30,6 +32,9 @@ function Type({ orderType }) {
         key={item.name}
         name={item.name}
         imagePath={item.imagePath}
+        updateItemCount={(itemName, newItemCount) =>
+          updateItemCount(itemName, newItemCount, orderType)
+        }
       />
     );
   });
@@ -37,7 +42,7 @@ function Type({ orderType }) {
     <>
       <h2>주문 종류</h2>
       <p>하나의 가격</p>
-      <p>상품 총 가격:</p>
+      <p>상품 총 가격: {orderDatas.totals[orderType]}</p>
       <div
         style={{
           display: 'flex',
